4ae4cb0394584ca1cb6ba7fedf6e74fdbe25e691
Alessandro Re <ale@ale-re.net>
2022-04-06 11:45:42 +0200

    feat: example pypi package for python-app

--- python-app/flake.nix
+++ python-app/flake.nix
@@ -1,3 +1,10 @@
+# This is a template with some examples showing how to use python with nix:
+# - the flake itself packages a python application,
+# - you can see in `my_py_env` and `my_py_env_dev` how to get (nix) python
+#   environments with dependencies, some exclusive to dev environment (nix develop)
+# - `pypi_package`: example on how to use a package from pypi instead of nixpkgs
+# - `my_py_tool`: example of python script to be used from the shell
+# - `my_help_script`: example of bash script useful for dev
 {
   description = "A python application";
   inputs = {
@@ -14,10 +21,35 @@
         # The packages to use as dependencies.
         pkgs = nixpkgs.legacyPackages.${system};
 
+        # Example package that is not in nixpkgs
+        pypi_package = pkgs.python39Packages.buildPythonPackage rec {
+          pname = "lark"; # example
+          version = "1.1.2";
+
+          # Fetch from pypi or use a path e.g. ./somepackage/
+          src = pkgs.python39Packages.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-eo0MB9Zj2pOR1/ruG/HX30mYxHykOlk8vvXHVmrNBXo=";
+          };
+
+          # Some build inputs might be necessary also at runtime
+          propagatedBuildInputs = [ pkgs.python39Packages.aiohttp ];
+
+          doCheck = false;
+
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/.../...";
+            description = "A package for doing things";
+            license = licenses.bsd3;
+            maintainers = [ "John Doe" ];
+          };
+        };
+
         # A python environment with some dependencies, used for runtime.
         my_py_env = pkgs.python3.withPackages (
           ps: with ps; [
             parse
+            pypi_package
           ]
         );
 
